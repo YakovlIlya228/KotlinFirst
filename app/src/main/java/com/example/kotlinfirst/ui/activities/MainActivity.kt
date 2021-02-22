2d672bdadfa07ef9d04b6c93907b753a8d9f694b
Ilya <jakovlilja@yandex.ru>
2021-02-22 16:31:25 +0300

    Added basic destinations in nav_graph.xml, added ViewBinding

--- app/src/main/java/com/example/kotlinfirst/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirst/ui/activities/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.kotlinfirst.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.kotlinfirst.R
+import com.example.kotlinfirst.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        view.navigation_view.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/example/kotlinfirst/ui/fragments/LessonListFragment.kt
+++ app/src/main/java/com/example/kotlinfirst/ui/fragments/LessonListFragment.kt
@@ -5,19 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.kotlinfirst.R
+import com.example.kotlinfirst.databinding.LessonListFragmentBinding
 
 class LessonListFragment: Fragment() {
+    private var _binding: LessonListFragmentBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.lesson_list_fragment, container, false)
+    ): View {
+        _binding = LessonListFragmentBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
