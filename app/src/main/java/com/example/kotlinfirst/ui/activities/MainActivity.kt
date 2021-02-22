2c9b21143846af2d7ff33030616ade232abc7342
Ilya <jakovlilja@yandex.ru>
2021-02-22 22:51:09 +0300

    Added basic layout views in code_editor_fragment.xml

--- app/src/main/java/com/example/kotlinfirst/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirst/ui/activities/MainActivity.kt
@@ -5,20 +5,19 @@ import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.kotlinfirst.R
-import com.example.kotlinfirst.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+        setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        view.navigation_view.setupWithNavController(navController)
+        navigation_view.setupWithNavController(navController)
 
     }
 }
--- app/src/main/java/com/example/kotlinfirst/ui/fragments/LessonListFragment.kt
+++ app/src/main/java/com/example/kotlinfirst/ui/fragments/LessonListFragment.kt
@@ -5,30 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.kotlinfirst.databinding.LessonListFragmentBinding
+import com.example.kotlinfirst.R
 
 class LessonListFragment: Fragment() {
-    private var _binding: LessonListFragmentBinding? = null
-
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = LessonListFragmentBinding.inflate(inflater,container,false)
-        return binding.root
+        return inflater.inflate(R.layout.lesson_list_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 
 
